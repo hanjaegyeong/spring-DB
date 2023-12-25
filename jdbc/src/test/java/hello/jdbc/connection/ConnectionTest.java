fd7f5267c50370064369f14b41c354ff19d1a02c
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-12-25 19:18:43 +0900

    feat: DataSource 예제2 - 커넥션 풀

--- jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
+++ jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -1,5 +1,6 @@
 package hello.jdbc.connection;
 
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -27,6 +28,21 @@ public class ConnectionTest {
         //DriverManagerDataSource - 항상 새로운 커넥션 획득
         DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
         useDataSource(dataSource);
+    }
+
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        //커넥션 풀링: HikariProxyConnection(Proxy) -> JdbcConnection(Target)
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10); //풀사이즈 지정 가능
+        dataSource.setPoolName("MyPool");
+
+        useDataSource(dataSource);
+
+        Thread.sleep(1000); //커넥션 풀에서 커넥션 생성 시간 대기 - 생성 로그 확인 위해
     }
 
     private void useDataSource(DataSource dataSource) throws SQLException {
