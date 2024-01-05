a46611dca7c72d50578bb60c0724416d3f515eee
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2024-01-05 16:45:23 +0900

    feat: MyBatis 적용3 - 동적프록시 확인

--- itemservice-db/src/main/java/hello/itemservice/repository/mybatis/MyBatisItemRepository.java
+++ itemservice-db/src/main/java/hello/itemservice/repository/mybatis/MyBatisItemRepository.java
@@ -5,10 +5,12 @@ import hello.itemservice.repository.ItemRepository;
 import hello.itemservice.repository.ItemSearchCond;
 import hello.itemservice.repository.ItemUpdateDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Repository
 @RequiredArgsConstructor
 public class MyBatisItemRepository implements ItemRepository {
@@ -17,6 +19,7 @@ public class MyBatisItemRepository implements ItemRepository {
 
     @Override
     public Item save(Item item) {
+        log.info("itemMapper class={}", itemMapper.getClass());
         itemMapper.save(item);
         return item;
     }
