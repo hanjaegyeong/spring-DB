7a809e2edb0c0e195f5988cc653dfe4d7d643889
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2024-01-14 21:52:59 +0900

    feat: 트랜잭션 전파 활용 - 예제 생성

---
+++ springtx/src/main/java/hello/springtx/propagation/Log.java
@@ -0,0 +1,23 @@
+package hello.springtx.propagation;
+
+import lombok.Getter;
+import lombok.Setter;
+import jakarta.persistence.*;
+
+@Entity
+@Getter @Setter
+public class Log {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String message;
+
+    public Log() {
+    }
+
+    public Log(String message) {
+        this.message = message;
+    }
+}
---
+++ springtx/src/main/java/hello/springtx/propagation/Member.java
@@ -0,0 +1,23 @@
+package hello.springtx.propagation;
+
+import lombok.Getter;
+import lombok.Setter;
+import jakarta.persistence.*;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String username;
+
+    public Member() {
+    }
+
+    public Member(String username) {
+        this.username = username;
+    }
+}
